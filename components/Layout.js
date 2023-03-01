28e2401ea01e9f23a9efc75bdec88ee9d37991cf
venkykoppoku <venky94.koppoku@gmail.com>
2023-03-01 17:13:30 +0530

    product details

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,33 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import React, { useContext } from 'react';
+import { Store } from '../utils/Store';
 
-const { default: Head } = require('next/head');
-
-const Layout = ({ title, children }) => {
+export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <>
       <Head>
-        <title>{title ? title + '- Amazon' : 'Amazon'}</title>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
         <meta name="description" content="Ecommerce Website" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex min-h-screen flex-col justify-between">
+
+      <div className="flex min-h-screen flex-col justify-between ">
         <header>
-          <nav className="flex justify-between px-4 items-center h-12 shadow-md">
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/" className="text-lg font-bold">
-              Amazon
+              amazona
             </Link>
             <div>
               <Link href="/cart" className="p-2">
                 Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
               </Link>
               <Link href="/login" className="p-2">
                 Login
@@ -27,13 +35,11 @@ const Layout = ({ title, children }) => {
             </div>
           </nav>
         </header>
-        <main className="container m-auto mt-4 px-4 ">{children}</main>
-        <footer className="flex justify-center items-center h-10 shadow-inner">
-          Copyright &#169; 2023 Amazon
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2023 Amazona</p>
         </footer>
       </div>
     </>
   );
-};
-
-export default Layout;
+}
