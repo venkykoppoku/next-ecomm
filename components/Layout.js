541cda9605e36492914e58ab691d6673eafac133
venkykoppoku <venky94.koppoku@gmail.com>
2023-03-01 22:22:07 +0530

    save cart items using cookie

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Store } from '../utils/Store';
 
 export default function Layout({ title, children }) {
+  const [cartItems, setCartItems] = useState([]);
   const { state } = useContext(Store);
   const { cart } = state;
+  useEffect(() => {
+    setCartItems(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
   return (
     <>
       <Head>
@@ -17,15 +22,13 @@ export default function Layout({ title, children }) {
       <div className="flex min-h-screen flex-col justify-between ">
         <header>
           <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-            <Link href="/" className="text-lg font-bold">
-              amazona
-            </Link>
+            <Link href="/">amazona</Link>
             <div>
               <Link href="/cart" className="p-2">
                 Cart
-                {cart.cartItems.length > 0 && (
+                {cartItems.length > 0 && (
                   <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    {cartItems}
                   </span>
                 )}
               </Link>
