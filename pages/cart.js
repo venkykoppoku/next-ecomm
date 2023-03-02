8f8a819b18d6b43ec7bdbc7c7d480cf7097c46f4
venkykoppoku <venky94.koppoku@gmail.com>
2023-03-02 13:24:13 +0530

    connect to mongodb

--- pages/cart.js
+++ pages/cart.js
@@ -99,7 +99,7 @@ function CartScreen() {
               <li>
                 <button
                   className="primary-button w-full"
-                  onClick={() => router.push('/shipping')}
+                  onClick={() => router.push('login?redirect=/shipping')}
                 >
                   {' '}
                   Check Out
